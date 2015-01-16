f5e5dd1860234a39e7c1318f4d676fa534c772d4
Kyle Mitchell <kyle@kemitchell.com>
2015-01-15 22:58:38 -0800

    Bower

--- test/module.test.js
+++ test/module.test.js
@@ -1,12 +1,30 @@
 /* jshint mocha: true */
 var semver = require('semver');
-require('chai').should();
+var expect = require('chai').expect;
 
-var metadata = require('../package.json');
+var npm = require('../package.json');
+var bower = require('../bower.json');
 
 describe('Module', function() {
   it('version is a valid semantic version', function() {
-    semver.valid(metadata.version)
-      .should.not.equal(null);
+    expect(semver.valid(npm.version))
+      .to.not.equal(null);
+  });
+
+  [
+    'author',
+    'description',
+    'homepage',
+    'keywords',
+    'license',
+    'main',
+    'name',
+    'repository',
+    'version'
+  ].forEach(function(key) {
+    it(key + ' is the same for npm and Bower', function() {
+      expect(npm[key])
+        .to.eql(bower[key]);
+    });
   });
 });
